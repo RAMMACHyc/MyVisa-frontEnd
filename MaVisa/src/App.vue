d290d3ed7bb8f59949ce6170256a7fac2930416b
RAMMACHyc <sadik.ram12@gmail.com>
2023-02-03 17:32:53 +0100

    frontEnd

---
+++ MaVisa/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Navbar from './components/navbar.vue'
+export default{
+  components:{
+    Navbar
+  }
+}
+ 
+</script>
+
+<template>
+  
+    <Navbar />
+</template>
+
+<style scoped>
+</style>
---
+++ MaVisa/src/components/navbar.vue
@@ -0,0 +1,42 @@
+<template>
+   
+<nav class="bg-white border-gray-200 dark:bg-red-800">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+        <a href="https://flowbite.com" class="flex items-center">
+            <img src="../images/Abstract Modern Mobile App Icon, tech company logo.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">MyVisa</span>
+        </a>
+        <div class="flex items-center">
+            <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">/</a>
+            <a href="#" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+        </div>
+    </div>
+</nav>
+<nav class="bg-gray-50 dark:bg-red-600">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto md:px-6">
+        <div class="flex items-center">
+            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Services</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">About</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</template>
+<script>
+export default {
+  name: "Navbar"
+}
+
+
+
+</script>
